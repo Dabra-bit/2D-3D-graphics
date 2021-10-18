01ed83c3ec88d0cceb3cb32259a8f352010a3f57
David Berrospe <david.berrospe.r03@gmail.com>
2021-10-18 14:42:49 -0500

    Changed default circle. Also new practice.

--- 2/pracs/Main.java
+++ 2/pracs/Main.java
@@ -5,8 +5,29 @@ import factories.LinesFactory;
 
 public class Main {
     public static void main(String[] args) {
-        CustomWindow cw = new CustomWindow("7th prac - Ellipse", 600, 600);
+        CustomWindow cw = new CustomWindow("8th prac - Figures", 1000, 600);
         cw.setColor(Color.RED);
-        cw.drawRectangle(100, 100, 50, 70);
+
+        // Lines
+        cw.drawLine(50, 50, 150, 150);
+        cw.drawLine(300, 100, 400, 100);
+        cw.drawLine(700, 50, 600, 150);
+        cw.drawLine(900, 100, 800, 100);
+
+        // Circles
+        cw.drawCircle(100, 450, 80);
+        cw.drawCircle(100, 450, 55);
+        cw.drawCircle(100, 450, 30);
+        cw.drawCircle(100, 450, 5);
+
+        // Rectangles
+        cw.drawRectangle(350, 400, 550, 500);
+        cw.drawRectangle(510, 470, 390, 430);
+
+        // Ellipses
+        cw.drawEllipse(800, 450, 10, 40);
+        cw.drawEllipse(800, 450, 20, 60);
+        cw.drawEllipse(800, 450, 30, 80);
+        cw.drawEllipse(800, 450, 40, 100);
     }
 }
--- 2/pracs/factories/CircleFactory.java
+++ 2/pracs/factories/CircleFactory.java
@@ -8,7 +8,7 @@ import points.CustomPoint;
 public class CircleFactory {
     public static final int BASIC_CIRCLE = 1;
     public static final int POLAR_COORDS_CIRCLE = 2;
-    public static final int DEFAULT_CIRCLE = BASIC_CIRCLE;
+    public static final int DEFAULT_CIRCLE = POLAR_COORDS_CIRCLE;
 
     public CustomCircle getCircle(CustomPoint center, int radius, int circleType) {
         if(circleType == 0) {
